25ea641cc5b1a0450e25645b276e45cbd6ea91a6
tanyj0612 <tanyijia9558@gmail.com>
2025-12-06 00:50:38 +0800

    feat : Add GenerateAttendanceCodeResponse.cs

---
+++ src/TuitionManagementSystem.Web/Features/Attendance/GenerateAttendanceCode/GenerateAttendanceCodeResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace TuitionManagementSystem.Web.Features.Attendance.GenerateAttendanceCode;
+
+public class GenerateAttendanceCodeResponse
+{
+    public int SessionId { get; set; }
+    public string Code { get; set; } = default!;
+    public DateOnly DatedFor { get; set; }
+}
+
+
