bc17b81397905586ce66fc6673ef0d16de7bdd09
tanyj0612 <tanyijia9558@gmail.com>
2025-12-06 14:49:17 +0800

    feat : add AttendanceController.cs

---
+++ src/TuitionManagementSystem.Web/Features/Attendance/AttendanceController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TuitionManagementSystem.Web.Features.Attendance;
+
+using Ardalis.Result;
+using Ardalis.Result.AspNetCore;
+using GenerateAttendanceCode;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+
+[Authorize]
+public class AttendanceController(IMediator mediator) : Controller
+{
+
+    [HttpPost("generate")]
+    [TranslateResultToActionResult]
+    public async Task<Result<GenerateAttendanceCodeResponse>> GenerateAttendanceCode(
+        [FromForm] string sessionId,
+        CancellationToken cancellationToken) => await mediator.Send(
+        new GenerateAttendanceCodeRequest(
+            sessionId),
+        cancellationToken);
+
+}
