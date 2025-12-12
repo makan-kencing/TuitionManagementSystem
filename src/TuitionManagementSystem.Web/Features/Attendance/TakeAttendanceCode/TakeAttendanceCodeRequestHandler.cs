9b5a33dd9d3e08f73de64ab60ac88fe0fb400be5
tanyj0612 <tanyijia9558@gmail.com>
2025-12-13 02:57:52 +0800

    fix : add the missing function

--- src/TuitionManagementSystem.Web/Features/Attendance/TakeAttendanceCode/TakeAttendanceCodeRequestHandler.cs
+++ src/TuitionManagementSystem.Web/Features/Attendance/TakeAttendanceCode/TakeAttendanceCodeRequestHandler.cs
@@ -6,6 +6,7 @@ using MediatR;
 using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore;
 using Models.Class;
+using Models.User;
 using Services.Auth.Extensions;
 
 public class TakeAttendanceCodeRequestHandler(ApplicationDbContext db ,IHttpContextAccessor httpContextAccessor)
@@ -17,32 +18,48 @@ public class TakeAttendanceCodeRequestHandler(ApplicationDbContext db ,IHttpCont
         CancellationToken cancellationToken)
     {
 
-        var session =await  db.Sessions
-            .Where(s=> s.Code.ToString()==request.Code)
+        var session = await db.Sessions
+            .Include(s => s.Code)
+            .Include(s => s.Course)
+            .Where(s => s.Code.Code == request.Code)
+            .OrderByDescending(s => s.StartAt)
+            .ThenByDescending(s => s.EndAt)
             .FirstOrDefaultAsync(cancellationToken);
-
+        var accountId = httpContextAccessor.HttpContext!.User.GetUserId();
+        var student =await db.Students
+            .Where(s=> s.Account.Id == accountId)
+            .FirstOrDefaultAsync(cancellationToken);
+        var currentEnrollment =await db.Enrollments
+            .Where(e=>e.Student.Id == student.Id)
+            .FirstOrDefaultAsync(cancellationToken);
+       var existingAttendance = await db.Attendances
+           .AnyAsync(a => a.Student.Id == student.Id
+                       && a.Session.Id == session.Id,
+                    cancellationToken);
         if (session == null)
         {
             return Result.NotFound("Attendance code not found");
         }
 
-        if (! this.CheckWithinTheTime(session) || !this.CheckWithEnrollment(session))
+        if (! this.CheckWithinTheTime(session) || !this.CheckWithEnrollment(currentEnrollment,session))
         {
             return Result.NotFound("Attendance not take in a time");
         }
 
-        var accountId = httpContextAccessor.HttpContext!.User.GetUserId();
-        var student =await db.Students
-            .Where(s=> s.Account.Id == accountId)
-            .FirstOrDefaultAsync(cancellationToken);
+        if (!existingAttendance)
+        {
+            return Result.NotFound("Attendance not take in a time");
+        }
+
 
         var attendance = new Attendance() { Session = session, Student = student, TakenOn = DateTime.UtcNow };
 
-        var addedAttendance = await db.Attendances.AddAsync(attendance,cancellationToken);
+       await db.Attendances.AddAsync(attendance,cancellationToken);
+       await db.SaveChangesAsync(cancellationToken);
 
         return Result<TakeAttendanceCodeResponse>.Success(new ()
         {
-            SessionId = session.Id.ToString()
+            SessionId = session.Code.Code
         });
     }
 
@@ -52,13 +69,10 @@ public class TakeAttendanceCodeRequestHandler(ApplicationDbContext db ,IHttpCont
         return currentTime >= session.StartAt && currentTime <= session.EndAt;
     }
 
-    private bool CheckWithEnrollment(Session session)
+    private bool CheckWithEnrollment(Enrollment currentEnrollment , Session session)
     {
-        if (session==null)
-        {
-            return false;
-        }
-        return true;
+        if (currentEnrollment.Course == session.Course) {return true;}
+        return false;
     }
 
 
