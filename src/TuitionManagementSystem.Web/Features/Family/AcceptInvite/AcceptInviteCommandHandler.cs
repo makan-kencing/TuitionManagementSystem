7e780f92100a685aa884d210350f88d65cf12fb5
makan-kencing <dizzynight2096@gmail.com>
2025-12-13 02:45:42 +0800

    feat: add logic to accept and decline invite

--- src/TuitionManagementSystem.Web/Features/Family/AcceptInvite/AcceptInviteCommandHandler.cs
+++ src/TuitionManagementSystem.Web/Features/Family/AcceptInvite/AcceptInviteCommandHandler.cs
@@ -1,13 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using TuitionManagementSystem.Web.Infrastructure.Persistence;
+using TuitionManagementSystem.Web.Models.Notification;
+using TuitionManagementSystem.Web.Services.Auth.Extensions;
+
 namespace TuitionManagementSystem.Web.Features.Family.AcceptInvite;
 
 using Ardalis.Result;
 using MediatR;
 
-public class AcceptInviteCommandHandler : IRequestHandler<AcceptInviteCommand, Result>
+public class AcceptInviteCommandHandler(
+    ApplicationDbContext db,
+    IHttpContextAccessor httpContextAccessor) : IRequestHandler<AcceptInviteCommand, Result>
 {
     public async Task<Result> Handle(AcceptInviteCommand command, CancellationToken cancellationToken)
     {
-        // TODO
-        throw new NotImplementedException();
+        var accountId = httpContextAccessor.HttpContext?.User.GetUserId();
+        if (accountId == null)
+        {
+            return Result.Unauthorized();
+        }
+
+        var invite = await db.FamilyInvites
+            .Where(i => i.User.Account.Id == accountId)
+            .Where(i => i.Status == InviteStatus.Pending)
+            .Include(i => i.User)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (invite is null)
+        {
+            return Result.NotFound();
+        }
+
+        invite.Status = InviteStatus.Accepted;
+        invite.User.Family = invite.Family;
+
+        await db.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
     }
 }
--- src/TuitionManagementSystem.Web/Features/Family/DeclineInvite/DeclineInviteCommandHandler.cs
+++ src/TuitionManagementSystem.Web/Features/Family/DeclineInvite/DeclineInviteCommandHandler.cs
@@ -1,13 +1,38 @@
 namespace TuitionManagementSystem.Web.Features.Family.DeclineInvite;
 
 using Ardalis.Result;
+using Infrastructure.Persistence;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Models.Notification;
+using Services.Auth.Extensions;
 
-public class DeclineInviteCommandHandler : IRequestHandler<DeclineInviteCommand, Result>
+public class DeclineInviteCommandHandler(
+    ApplicationDbContext db,
+    IHttpContextAccessor httpContextAccessor) : IRequestHandler<DeclineInviteCommand, Result>
 {
-    public Task<Result> Handle(DeclineInviteCommand command, CancellationToken cancellationToken)
+    public async Task<Result> Handle(DeclineInviteCommand command, CancellationToken cancellationToken)
     {
-        // TODO
-        throw new NotImplementedException();
+        var accountId = httpContextAccessor.HttpContext?.User.GetUserId();
+        if (accountId == null)
+        {
+            return Result.Unauthorized();
+        }
+
+        var invite = await db.FamilyInvites
+            .Where(i => i.User.Account.Id == accountId)
+            .Where(i => i.Status == InviteStatus.Pending)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (invite is null)
+        {
+            return Result.NotFound();
+        }
+
+        invite.Status = InviteStatus.Declined;
+
+        await db.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
     }
 }
