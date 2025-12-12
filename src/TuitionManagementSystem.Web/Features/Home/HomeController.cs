7bc0f3d28d4675eb4221d1949e3fd1c6425ffdbd
makan-kencing <dizzynight2096@gmail.com>
2025-12-12 17:44:00 +0800

    feat: wip home dashboard

--- src/TuitionManagementSystem.Web/Features/Home/HomeController.cs
+++ src/TuitionManagementSystem.Web/Features/Home/HomeController.cs
@@ -1,15 +1,31 @@
 namespace TuitionManagementSystem.Web.Features.Home;
 
 using System.Diagnostics;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Models.User;
+using Services.Auth.Extensions;
 
-public class HomeController(ILogger<HomeController> logger) : Controller
+public class HomeController(IMediator mediator) : Controller
 {
-    public IActionResult Index() => this.View();
-
-    public IActionResult Privacy() => this.View();
     [AllowAnonymous]
+    public async Task<IActionResult> Index()
+    {
+        switch (this.User.GetUserType())
+        {
+            case nameof(Student):
+                return this.View("StudentDashboard");
+            case nameof(Teacher):
+                return this.View("TeacherDashboard");
+            case nameof(Parent):
+                return this.View("ParentDashboard");
+            default:
+                return this.View("GuestDashboard");
+        }
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error() => this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+    public IActionResult Error() =>
+        this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
 }
