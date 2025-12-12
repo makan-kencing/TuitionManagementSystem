3168a4d099e4e18dbf34210bbd70a8c389936403
tanyj0612 <tanyijia9558@gmail.com>
2025-12-13 01:10:55 +0800

    feat : add TakeAttendanceCodeRequest.cs

---
+++ src/TuitionManagementSystem.Web/Features/Attendance/TakeAttendanceCode/TakeAttendanceCodeRequest.cs
@@ -0,0 +1,5 @@
+﻿namespace TuitionManagementSystem.Web.Features.Attendance.TakeAttendanceCode;
+using Ardalis.Result;
+using MediatR;
+public record TakeAttendanceCodeRequest(string Code)
+    : IRequest<Result<TakeAttendanceCodeResponse>>;
