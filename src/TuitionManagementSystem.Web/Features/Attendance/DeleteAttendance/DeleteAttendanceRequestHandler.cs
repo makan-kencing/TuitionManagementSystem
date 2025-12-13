33d72fecd0c022c2eddce9b95f67afba369f5b87
tanyj0612 <tanyijia9558@gmail.com>
2025-12-13 22:47:19 +0800

    feat : add DeleteAttendanceRequestHandler.cs

---
+++ src/TuitionManagementSystem.Web/Features/Attendance/DeleteAttendance/DeleteAttendanceRequestHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace TuitionManagementSystem.Web.Features.Attendance.DeleteAttendance;
+
+using Ardalis.Result;
+using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+public sealed class DeleteAttendanceRequestHandler(ApplicationDbContext db
+    , IHttpContextAccessor httpContextAccessor) :
+    IRequestHandler<DeleteAttendanceRequest, Result<DeleteAttendanceResponse>>
+{
+    public async Task<Result<DeleteAttendanceResponse>> Handle(
+        DeleteAttendanceRequest request, CancellationToken cancellationToken)
+    {
+        var checkAttendance = await db.Attendances
+            .Where(ad => ad.Id == request.AttendanceId)
+            .Where(ad=>ad.Student.Id == request.StudentId)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (checkAttendance==null)
+        {
+            return Result.Invalid();
+        }
+
+        db.Attendances.Remove(checkAttendance);
+        await db.SaveChangesAsync(cancellationToken);
+
+        return Result<DeleteAttendanceResponse>.Success(new DeleteAttendanceResponse());
+
+    }
+}
