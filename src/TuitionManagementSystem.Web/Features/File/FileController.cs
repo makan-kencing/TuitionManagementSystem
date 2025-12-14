df125ff8b13487e7feeb59d61910f6d369a55c18
makan-kencing <dizzynight2096@gmail.com>
2025-12-15 02:37:13 +0800

    feat: delete file

--- src/TuitionManagementSystem.Web/Features/File/FileController.cs
+++ src/TuitionManagementSystem.Web/Features/File/FileController.cs
@@ -2,13 +2,19 @@ namespace TuitionManagementSystem.Web.Features.File;
 
 using Abstractions;
 using Ardalis.Result;
+using DeleteFile;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using UploadFiles;
 
-public class FileApiController(IMediator mediator) : ApiController
+public class FileController(IMediator mediator) : ApiController
 {
     [HttpPost]
     public async Task<Result<UploadFilesResponse>> Upload(IFormFileCollection files) =>
         await mediator.Send(new UploadFilesCommand(files));
+
+    [HttpDelete]
+    [Route("{id:int:required}")]
+    public async Task<Result> Delete(int id) =>
+        await mediator.Send(new DeleteFileCommand(id));
 }
