615b06366d1d41859a30a5460c12bf56dbd9d0fe
tanyj0612 <tanyijia9558@gmail.com>
2025-12-14 20:23:08 +0800

    feat : add View model AttendanceSummaryViewModel.cs and CourseSummaryViewModel.cs

---
+++ src/TuitionManagementSystem.Web/Features/Attendance/AttendanceSummary/AttendanceSummaryViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace TuitionManagementSystem.Web.Features.Attendance.AttendanceSummary;
+
+public class AttendanceSummaryViewModel
+{
+    public int StudentId { get; set; }
+    public string StudentName { get; set; }
+
+    public decimal OverallAttendanceRate { get; set; }
+
+    public int TotalSessions { get; set; }
+
+    public int AttendedSessions { get; set; }
+    public List<CourseSummaryViewModel> Courses { get; set; }
+}
+
+
