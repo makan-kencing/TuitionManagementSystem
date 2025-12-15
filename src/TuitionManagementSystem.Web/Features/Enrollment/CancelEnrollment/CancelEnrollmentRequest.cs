0d49e5a99d283ae78307f46612e48a27847b9830
Lee <leebincent@gmail.com>
2025-12-15 23:31:06 +0800

    Removed CancelEnrollment

--- src/TuitionManagementSystem.Web/Features/Enrollment/CancelEnrollment/CancelEnrollmentRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace TuitionManagementSystem.Web.Features.Enrollment.CancelEnrollment;
-
-using Ardalis.Result;
-using MediatR;
-
-public record CancelEnrollmentRequest(int EnrollmentId)
-    : IRequest<Result>;
