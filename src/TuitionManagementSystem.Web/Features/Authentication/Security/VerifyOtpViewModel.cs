0bef12675929a53afe292b715eb3a3f49a027011
WHannnnnnn <whbeh11@gmail.com>
2025-12-15 15:57:45 +0800

    Added OtpVM

---
+++ src/TuitionManagementSystem.Web/Features/Authentication/Security/VerifyOtpViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace TuitionManagementSystem.Web.Features.Authentication.Security;
+
+public class VerifyOtpViewModel
+{
+    public string Otp { get; set; } = null!;
+    public string? Message { get; set; }
+}
