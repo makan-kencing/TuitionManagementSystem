daa43eb643bb10c635dcf3487030ee46e57090e3
WHannnnnnn <whbeh11@gmail.com>
2025-12-15 15:59:37 +0800

    added VM for newpassword and forgotpassword

---
+++ src/TuitionManagementSystem.Web/Features/Authentication/Security/ForgotPasswordViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TuitionManagementSystem.Web.Features.Authentication.Security;
+
+using System.ComponentModel.DataAnnotations;
+
+public class ForgotPasswordViewModel
+{
+    [Required]
+    [EmailAddress]
+    public string? Email { get; set; }
+
+    public string? Otp { get; set; }
+
+    public bool ShowOtpForm { get; set; } = false;
+
+}
---
+++ src/TuitionManagementSystem.Web/Features/Authentication/Security/NewPasswordViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TuitionManagementSystem.Web.Features.Authentication.Security;
+
+using System.ComponentModel.DataAnnotations;
+
+public class NewPasswordViewModel
+{
+    [Required]
+    [DataType(DataType.Password)]
+    public string NewPassword { get; set; } = "";
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; } = "";
+}
