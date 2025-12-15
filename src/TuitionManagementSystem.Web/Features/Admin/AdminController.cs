11cda718c6ec06abc4adf601b719a5383640ae71
WHannnnnnn <whbeh11@gmail.com>
2025-12-15 16:00:39 +0800

    Added Authentication for admin

--- src/TuitionManagementSystem.Web/Features/Admin/AdminController.cs
+++ src/TuitionManagementSystem.Web/Features/Admin/AdminController.cs
@@ -2,8 +2,9 @@ namespace TuitionManagementSystem.Web.Features.Admin;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Services.Auth.Constants;
 
-[Authorize]
+[Authorize(Roles = nameof(AccessRoles.Administrator))]
 public class AdminController : Controller
 {
     public IActionResult Index() => this.View();
