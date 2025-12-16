57b8e71a7df4cbbfa7e076026affa391e83afe2d
WHannnnnnn <whbeh11@gmail.com>
2025-12-17 02:48:55 +0800

    Added viewmodel for accountprofile

---
+++ src/TuitionManagementSystem.Web/Features/Accounts/AccountProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TuitionManagementSystem.Web.Features.Accounts;
+
+using Microsoft.AspNetCore.Mvc.Razor;
+
+public class AccountProfileViewModel
+{
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
