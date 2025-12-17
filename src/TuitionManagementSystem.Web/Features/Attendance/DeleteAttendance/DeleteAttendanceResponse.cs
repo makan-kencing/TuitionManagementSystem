d0d345aa42a3b22739a68f5c26f5a59293ad50a3
tanyj0612 <tanyijia9558@gmail.com>
2025-12-17 20:14:18 +0800

    feat : add for htmx

---
+++ src/TuitionManagementSystem.Web/Features/Attendance/DeleteAttendance/DeleteAttendanceResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace TuitionManagementSystem.Web.Features.Attendance.DeleteAttendance;
+
+public class DeleteAttendanceResponse
+{
+    public int UserId{ get; set; }
+    public int SessionId{ get; set; }
+
+    public required DateTime CurrentAt{ get; set; }
+
+    public SessionTime? SessionTime { get; set; }
+}
+
+public class SessionTime
+{
+    public required DateTime StartAt{ get; set; }
+    public required DateTime EndAt{ get; set; }
+
+}
