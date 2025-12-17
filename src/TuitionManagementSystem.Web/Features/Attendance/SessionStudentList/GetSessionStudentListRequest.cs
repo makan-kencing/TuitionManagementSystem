78dbc6395788347579b2293343d6d64f39924b97
tanyj0612 <tanyijia9558@gmail.com>
2025-12-17 14:05:50 +0800

    feat : add GetSessionStudentListRequest.cs

---
+++ src/TuitionManagementSystem.Web/Features/Attendance/SessionStudentList/GetSessionStudentListRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TuitionManagementSystem.Web.Features.Attendance.SessionStudentList;
+
+using Ardalis.Result;
+using MediatR;
+
+public record GetSessionStudentListRequest(int SessionId)
+    : IRequest<Result<GetSessionStudentListResponse>>;
