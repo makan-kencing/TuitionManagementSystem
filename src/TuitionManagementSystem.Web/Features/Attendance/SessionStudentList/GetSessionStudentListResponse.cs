ac97f7f880035b8f63d6620b93493b120c8ba9c8
tanyj0612 <tanyijia9558@gmail.com>
2025-12-17 17:58:11 +0800

    feat : add two varables

--- src/TuitionManagementSystem.Web/Features/Attendance/SessionStudentList/GetSessionStudentListResponse.cs
+++ src/TuitionManagementSystem.Web/Features/Attendance/SessionStudentList/GetSessionStudentListResponse.cs
@@ -3,13 +3,16 @@
 public class GetSessionStudentListResponse
 {
     public List<StudentInfo> StudentList { get; set; } = [];
-
+    public  int SessionId { get; set; }
 }
 
 public class StudentInfo
 {
+
    public int  StudentId  { get; set; }
    public required string Name  { get; set; }
+
+   public int? AttendanceId { get; set; }
    public string Status { get; set; } = default!;
 
 }
