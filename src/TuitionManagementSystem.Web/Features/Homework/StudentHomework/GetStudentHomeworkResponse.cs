20279abd28fd85a27e04273652f75bfce81d96e2
tanyj0612 <tanyijia9558@gmail.com>
2025-12-18 01:29:02 +0800

    feat add the menu for the homework (student side)

---
+++ src/TuitionManagementSystem.Web/Features/Homework/StudentHomework/GetStudentHomeworkResponse.cs
@@ -0,0 +1,33 @@
+﻿namespace TuitionManagementSystem.Web.Features.Homework.StudentHomework;
+
+public class GetStudentHomeworkResponse
+{
+    public string? Name { get; set; }
+    public List<StudentCoursesInfo> CourseInfos { get; set; } = [];
+    public List<StudentEnrollInfo> EnrollInfos { get; set; } = [];
+}
+
+public class StudentEnrollInfo
+{
+    public int CourseId { get; set; }
+    public DateTime EnrollDate { get; set; }
+}
+
+public class StudentCoursesInfo
+{
+    public string? CourseName { get; set; }
+
+    public string?SubjectName { get; set; }
+    public int TeacherId { get; set; }
+    public string? TeacherName { get; set; }
+    public List<StudentCourseSection> CourseSections { get; set; } = [];
+}
+
+
+public class StudentCourseSection
+{
+    public int SessionId { get; set; }
+    public DateTime StratAt { get; set; }
+    public DateTime EndAt { get; set; }
+
+}
