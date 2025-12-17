19467630b41668de8eb01c9728984bc6bc66b2d8
tanyj0612 <tanyijia9558@gmail.com>
2025-12-17 14:06:24 +0800

    feat : Add GetSessionStudentListResponse.cs

---
+++ src/TuitionManagementSystem.Web/Features/Attendance/SessionStudentList/GetSessionStudentListResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace TuitionManagementSystem.Web.Features.Attendance.SessionStudentList;
+
+public class GetSessionStudentListResponse
+{
+    public List<StudentInfo> StudentList { get; set; } = [];
+
+}
+
+public class StudentInfo
+{
+   public int  StudentId  { get; set; }
+   public required string Name  { get; set; }
+   public string Status { get; set; } = default!;
+
+}
