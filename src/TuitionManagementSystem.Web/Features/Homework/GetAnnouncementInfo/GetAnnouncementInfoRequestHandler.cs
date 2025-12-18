0a75112defdd740d8a8b635accf274213e93cab9
tanyj0612 <tanyijia9558@gmail.com>
2025-12-19 01:31:35 +0800

    feat : add GetAnnouncementInfoRequestHandler.cs and GetAnnouncementInfoResponse.cs

---
+++ src/TuitionManagementSystem.Web/Features/Homework/GetAnnouncementInfo/GetAnnouncementInfoRequestHandler.cs
@@ -0,0 +1,54 @@
+﻿namespace TuitionManagementSystem.Web.Features.Homework.GetAnnouncementInfo;
+
+using Ardalis.Result;
+using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Models.Class.Announcement;
+using TeacherHomework;
+
+public sealed class GetAnnouncementInfoRequestHandler(ApplicationDbContext db)
+    : IRequestHandler<GetAnnouncementInfoRequest, Result<GetAnnouncementInfoResponse>>
+{
+    public async Task<Result<GetAnnouncementInfoResponse>> Handle(GetAnnouncementInfoRequest request,
+        CancellationToken cancellationToken)
+    {
+        var announcements = await db.Announcements.OfType<Assignment>()
+            .Where(an => an.CreatedById == request.UserId)
+            .Select(an=> new AnnouncementInfo
+            {
+                Title =  an.Title,
+                Description =  an.Description,
+                CreatedAt = an.CreatedAt,
+                UpdatedAt = an.UpdatedAt,
+                PublishedAt = an.PublishedAt,
+                DueAt = an.DueAt
+            })
+            .ToListAsync(cancellationToken);
+
+        var now = DateTime.UtcNow;
+
+        var teacherName = await db.Accounts
+            .Where(a => a.User.Id == request.UserId)
+            .Select(a => a.DisplayName)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        var courseInfo = await db.Courses
+            .Where(c => c.Id == request.CourseId)
+            .Select(c=> new CourseInfo
+            {
+                CourseName = c.Name,
+                Subject = c.Subject.Name
+            })
+            .FirstOrDefaultAsync(cancellationToken);
+
+        var announcementInfomation = new GetAnnouncementInfoResponse
+        {
+            AnnouncementInfos = announcements, TeacherName = teacherName ,CourseInfo = courseInfo ,Now = now
+        };
+
+        return Result<GetAnnouncementInfoResponse>.Success(announcementInfomation);
+
+    }
+
+}
---
+++ src/TuitionManagementSystem.Web/Features/Homework/GetAnnouncementInfo/GetAnnouncementInfoResponse.cs
@@ -0,0 +1,34 @@
+﻿namespace TuitionManagementSystem.Web.Features.Homework.GetAnnouncementInfo;
+
+public class GetAnnouncementInfoResponse
+{
+    public required string TeacherName { get; set; }
+
+    public required CourseInfo CourseInfo { get; set; }
+  public required List<AnnouncementInfo> AnnouncementInfos { get; set; }
+  public DateTime Now { get; set; }
+}
+
+public class AnnouncementInfo
+{
+    public required string Title { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public DateTime? PublishedAt { get; set; }
+
+    public DateTime? DueAt { get; set; }
+
+}
+
+public class CourseInfo
+{
+    public string? CourseName { get; set; }
+    public string? Subject { get; set; }
+}
+
+
