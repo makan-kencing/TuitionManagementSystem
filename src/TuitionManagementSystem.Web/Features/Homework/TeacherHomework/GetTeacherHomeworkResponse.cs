487c15be6c29a682fb0800ec9e497804964537b5
tanyj0612 <tanyijia9558@gmail.com>
2025-12-18 14:14:44 +0800

    feat : GetTeacherHomeworkResponse.cs

---
+++ src/TuitionManagementSystem.Web/Features/Homework/TeacherHomework/GetTeacherHomeworkResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace TuitionManagementSystem.Web.Features.Homework.TeacherHomework;
+
+public class GetTeacherHomeworkResponse
+{
+    public TeacherInfo ? TeacherInfo { get; set; }
+    public List<CourseInfo> CourseInfos { get; set; } = [];
+}
+
+public class TeacherInfo
+{
+    public string? TeacherName{get;set;}
+    public int TeacherId{get;set;}
+}
+
+public class CourseInfo
+{
+    public string? CourseName{get;set;}
+    public int CourseId{get;set;}
+    public string? SubjectName{get;set;}
+
+}
