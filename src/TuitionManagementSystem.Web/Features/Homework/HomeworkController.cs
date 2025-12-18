f18c1adceb1f228b7a919a2fa4a137ff0ea4ec29
tanyj0612 <tanyijia9558@gmail.com>
2025-12-18 15:30:49 +0800

    feat : add controller for teacher part

--- src/TuitionManagementSystem.Web/Features/Homework/HomeworkController.cs
+++ src/TuitionManagementSystem.Web/Features/Homework/HomeworkController.cs
@@ -6,6 +6,7 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Services.Auth.Extensions;
 using StudentHomework;
+using TeacherHomework;
 
 public class HomeworkController (IMediator mediator, ApplicationDbContext db) : Controller
 {
@@ -18,6 +19,22 @@ public class HomeworkController (IMediator mediator, ApplicationDbContext db) : 
         }
 
         var result = await mediator.Send(new GetStudentHomeworkRequest(userId), cancellationToken);
+        if (result.IsNotFound())
+        {
+            return this.NotFound();
+        }
+        return this.View(result.Value);
+    }
+
+    public async Task<IActionResult> HomeworkManageMenu(CancellationToken cancellationToken)
+    {
+        var userId = this.User.GetUserId();
+        if (userId == -1)
+        {
+            return this.Unauthorized();
+        }
+
+        var result = await mediator.Send(new GetTeacherHomeworkRequest(userId), cancellationToken);
         if (result.IsNotFound())
         {
             return this.NotFound();
