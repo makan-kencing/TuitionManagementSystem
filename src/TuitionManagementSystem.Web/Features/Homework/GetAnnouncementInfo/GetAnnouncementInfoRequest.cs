b16a505dca6c303cc6c66ee7f94b3d674b8b0605
tanyj0612 <tanyijia9558@gmail.com>
2025-12-19 01:31:06 +0800

    feat : add GetAnnouncementInfoRequest.cs

---
+++ src/TuitionManagementSystem.Web/Features/Homework/GetAnnouncementInfo/GetAnnouncementInfoRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace TuitionManagementSystem.Web.Features.Homework.GetAnnouncementInfo;
+
+using Ardalis.Result;
+using MediatR;
+
+public record GetAnnouncementInfoRequest(int UserId,int CourseId)
+    : IRequest<Result<GetAnnouncementInfoResponse>>;
