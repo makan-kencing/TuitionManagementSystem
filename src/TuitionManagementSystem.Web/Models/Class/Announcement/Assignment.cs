d423be6d7b3102761bf545fb8eca2501de55204c
tanyj0612 <tanyijia9558@gmail.com>
2025-12-19 21:17:31 +0800

    feat : add assigment submissions

--- src/TuitionManagementSystem.Web/Models/Class/Announcement/Assignment.cs
+++ src/TuitionManagementSystem.Web/Models/Class/Announcement/Assignment.cs
@@ -3,4 +3,6 @@ namespace TuitionManagementSystem.Web.Models.Class.Announcement;
 public class Assignment : Announcement
 {
     public DateTime? DueAt { get; set; }
+
+    public ICollection<Submission> Submissions { get; set; } = [];
 }
