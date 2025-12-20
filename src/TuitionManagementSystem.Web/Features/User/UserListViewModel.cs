7c91a7d522bb78547acec7ca9e5d1d6cff32c68b
WHannnnnnn <whbeh11@gmail.com>
2025-12-20 21:16:41 +0800

    Added userlist and userprofile viewmodel

---
+++ src/TuitionManagementSystem.Web/Features/User/UserListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TuitionManagementSystem.Web.Features.User;
+
+public class UserListViewModel
+{
+        public int Id { get; set; }
+        public string Username { get; set; } = default!;
+        public string? DisplayName { get; set; }
+        public string Email { get; set; } = default!;
+}
---
+++ src/TuitionManagementSystem.Web/Features/User/UserProfileViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace TuitionManagementSystem.Web.Features.User;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+public class UserProfileViewModel
+{
+    public int UserId { get; set; } // Needed to identify the user
+
+    [Required]
+    [DisplayName("Username")]
+    [Remote("CheckUsernameUnique", "AccountsApi", AdditionalFields = "UserId", ErrorMessage = "Username is taken")]
+    public required string Username { get; set; }
+
+    [DisplayName("Display Name")]
+    [StringLength(50)]
+    public string? DisplayName { get; set; }
+
+    [DisplayName("Email")]
+    [EmailAddress]
+    [Remote("CheckEmailUnique", "AccountsApi", AdditionalFields = "UserId", ErrorMessage = "Email is taken")]
+    public string? Email { get; set; } = string.Empty;
+
+    public string? ProfileImageUrl { get; set; }
+
+    [DisplayName("Profile Image")]
+    public IFormFile? ProfileImage { get; set; }
+}
